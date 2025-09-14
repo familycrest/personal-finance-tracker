e177e6da260fec03940852524635612570827fc7
Layla Shuemate <92069218+LaylaScout@users.noreply.github.com>
2025-09-14 15:22:45 -0400

    Update urls.py
    PersonalFinanceTrackerProject/urls.py code from Layla's project creation.

--- personal_finance_tracker/urls.py
+++ personal_finance_tracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for personal_finance_tracker project.
+URL configuration for PersonalFinanceTrackerProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# PersonalFinanceTrackerProject/urls.py
+
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from accounts import views
+from . import views as project_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    # Public homepage
+    path("", project_views.home, name="home"),
+
+    # Dashboard after login
+    path("dashboard/", project_views.dashboard, name="dashboard"),
+
+    # Accounts
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page='home'), name="logout"),
 ]
+
+
