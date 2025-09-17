18d7ae6277a114e3c4885dc3af99ce6b4741f15f
Kenneth Stancil <githubbeded@proton.me>
2025-09-17 15:27:06 -0400

    Remove parameter "next_page='home'" from accounts/urls.py due to LOGIN_REDIRECT_URL being specified in settings.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page='home'), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
