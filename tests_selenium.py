d780b3ed83636d73f0dcad2ef58675c7cefff052
Colton Stotler <coltonstotler0@gmail.com>
2025-09-21 17:21:57 -0400

    Commented out version check function.

--- tests_selenium.py
+++ tests_selenium.py
@@ -84,17 +84,17 @@ class TestAllPagesAvailable(LiveServerTestCase):
         super().tearDownClass()
         cls.driver.quit()
 
-    def check_driver_vs_chrome_version(self):
-        str1 = self.driver.capabilities["browserVersion"]
-        str2 = self.driver.capabilities["chrome"]["chromedriverVersion"].split(" ")[0]
-        print(str1)
-        print(str2)
-        print(str1[0:2])
-        print(str2[0:2])
-        if str1[0:2] != str2[0:2]:
-            print(
-                "Incorrect chrome driver version installed, please go kick Colton and get him to actually fix it this time."
-            )
+    # def check_driver_vs_chrome_version(self):
+    #     str1 = self.driver.capabilities["browserVersion"]
+    #     str2 = self.driver.capabilities["chrome"]["chromedriverVersion"].split(" ")[0]
+    #     print(str1)
+    #     print(str2)
+    #     print(str1[0:2])
+    #     print(str2[0:2])
+    #     if str1[0:2] != str2[0:2]:
+    #         print(
+    #             "Incorrect chrome driver version installed, please go kick Colton and get him to actually fix it this time."
+    #         )
 
     def check_page_available(self, path, expected_text):
         url = self.live_server_url + path
