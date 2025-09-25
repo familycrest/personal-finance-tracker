1a82a3a7a5ff873ed4d8eb544963b37637f05266
skybldev <skybluehexapon@outlook.com>
2025-09-24 21:12:33 -0400

    have settings.py read secrets from .env file
    - .env file added to store secret key and debug flag
    - `python-dotenv` added in order to read .env file
    - settings.py changed to use envars
    - `djangorestframework` added for future rest routes

--- personal_finance_tracker/settings.py
+++ personal_finance_tracker/settings.py
@@ -11,22 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a8l&sme_dmvgtoub+!rp2a5dejz6no-*o*(fbn-fxo^p9yyymo"  # DO NOT USE THIS KEY IN PRODUCTION! "django-insecure-a8l&sme_dmvgtoub+!rp2a5dejz6no-*o*(fbn-fxo^p9yyymo" AT TIME OF WRITING THIS COMMENT
+# If this causes an error, you need to have a .env file with the secret key in it.
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DJANGO_DEBUG") == "true" else False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [] if DEBUG else ["127.0.0.1"] 
 
 # Application definition
 
