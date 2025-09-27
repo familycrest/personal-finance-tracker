9a804e999039d8fa775dff3f2ee5aa78d8b3bcc8
Kenneth Stancil <githubbeded@proton.me>
2025-09-27 14:17:20 -0400

    Change structure

--- tests/tests_selenium.py
+++ tests/tests_selenium.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.core.os_manager import ChromeType
+# from webdriver_manager.core.os_manager import ChromeType <--------------------------------------------------------------
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys <-----------------------------------------------------------
 from selenium.webdriver.common.by import By
 import time
 
