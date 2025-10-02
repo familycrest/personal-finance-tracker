2d175579aad6343ef03490bc7175c8fef3bd8f6a
Kenneth Stancil <githubbeded@proton.me>
2025-10-01 21:15:38 -0400

    Revert .env.example and fix merge conflict with forms.py.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,8 @@
-# accounts/forms.py
-# this was created because we created a custom user model.
-# Django uses default settings for auth.user.
-# You need to make a form to create a custom user for Django
-# to recognize.
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
+# Temporary fix for changing auth model from User to UserAccount
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ("username", "email") # may add "email" later
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email')
