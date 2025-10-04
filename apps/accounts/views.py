ab4f3d84c0a08dcd7debf0b41cf36a1d8ebddcde
Kenneth Stancil <githubbeded@proton.me>
2025-10-04 15:46:04 -0400

    Remove debugging print statements, add removed ids back to elements to fix tests

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -4,30 +4,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseServerError
 
 def signup(request):
-    print("i just received a request!")
-
     # Redirect the user to the dashboard if they're already logged in
     if request.user.is_authenticated:
-        print("the user is already logged in though, so no need for them to sign up. sending them to their dashboard!")
-
         return redirect("dashboard")
     
     user = get_user_model()
     if request.method == "POST":
-        
-        print("request is a POST, so that means that it's a form submission!")
 
         form = CustomUserCreationForm(request.POST)
         
         if form.is_valid():
-            print("yay, the form is valid!")
-
             user = form.save()  # save the new user
             login(request, user)  # log the user in automatically
             return redirect("dashboard")  # redirect to dashboard
         else:
-            print("unfortunately, the form was invalid :(")
-            
+            pass
             # TODO: Tell the user exactly why it was invalid
 
     else:
--- base/views.py
+++ base/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    print("requested home...")
     return render(request, "home.html")
 
 
