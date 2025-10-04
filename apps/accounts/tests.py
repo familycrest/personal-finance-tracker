f162e1dd491951a44ba185f3d7a9bb70da5b1207
Kenneth Stancil <githubbeded@proton.me>
2025-10-04 15:05:56 -0400

    Fix import in tests.py in accounts app

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from personal_finance_tracker.tests import TestHelper
+from base.tests import TestHelper
 from itertools import product
 
 
