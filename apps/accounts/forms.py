e92b048b5e2faeaa4dd8e93d171bf1e6483a4275
skybldev <skybluehexapon@outlook.com>
2025-10-04 09:26:48 -0400

    disable autocomplete on verification form (forget OTP codes)

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from django.forms import widgets
 
 
 # Temporary fix for changing auth model from User to UserAccount
@@ -10,7 +11,13 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email')
         
 class EmailVerificationForm(forms.Form):
-    code = forms.CharField(label="Enter code (case insensitive)", max_length=6)
+    # a set-length text field with disabled autocomplete/suggestions
+    # (do not remember previous codes)
+    code = forms.CharField(
+        label="Enter code (case insensitive)",
+        max_length=6,
+        widget=widgets.TextInput(attrs={"autocomplete": "off"})
+    )
 
 class TestLoginForm(forms.Form):
     email = forms.EmailField(label="Enter your email")
