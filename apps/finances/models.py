a11484026586ddb6f0ed7d674f8f36de54d52018
Colton Stotler <coltonstotler0@gmail.com>
2025-10-08 14:03:29 -0400

    Added back Categories methods.

--- apps/finances/models.py
+++ apps/finances/models.py
@@ -30,6 +30,112 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_entry_type(self):
+        return self.entry_type
+
+    def get_entries(self):
+        return self.entry_set.all()
+
+    def add_entry(
+        self,
+        name: str,
+        description: str,
+        entry_type: EntryType,
+        date: str,
+        amount: Decimal,
+    ):
+        try:
+            # TODO: Add input checking here
+            entry = Entry(
+                user=self.user,
+                category=self,
+                name=name,
+                description=description,
+                entry_type=entry_type,
+                date=date,
+                amount=amount,
+            )
+            entry.save()
+            return entry
+        except Exception:
+            return None
+
+    def remove_entry(self, name: str):
+        try:
+            entry = self.entry_set.get(name=name)
+            entry.delete()
+            return True
+        except Entry.DoesNotExist:
+            return False
+
+    def get_goals(self):
+        return self.categorygoal_set.all()
+
+    def add_goal(
+        self,
+        name: str,
+        description: str,
+        entry_type: EntryType,
+        start_date: str,
+        end_date: str,
+        amount: Decimal,
+    ):
+        try:
+            # TODO: Add input checking here
+            goal = CategoryGoal(
+                category=self,
+                name=name,
+                description=description,
+                entry_type=entry_type,
+                start_date=start_date,
+                end_date=end_date,
+                amount=amount,
+            )
+            goal.save()
+            return goal
+        except Exception:
+            return None
+
+    def remove_goal(self, name: str):
+        try:
+            goal = self.categorygoal_set.get(name=name)
+            goal.delete()
+            return True
+        except CategoryGoal.DoesNotExist:
+            return False
+
+    def get_total(self):
+        entries = self.get_entries()
+        total = sum(
+            (entry.amount for entry in entries if entry.entry_type == self.entry_type),
+            Decimal("0"),
+        )
+        return total
+
+    def get_goal_percentages(self):
+        goals = self.get_goals()
+        if not goals:
+            return {}  # No goals set
+
+        goal_percentages = {}
+
+        for goal in goals:
+            total_entries_amount = self.get_total()
+            total_goal_amount = goal.amount
+            if total_goal_amount == 0:
+                goal_percentages[goal] = Decimal("0")
+            else:
+                percentage = (total_entries_amount / total_goal_amount) * Decimal("100")
+                goal_percentages[goal] = percentage
+
+        return goal_percentages
+
 
 # Entry model
 class Entry(models.Model):
