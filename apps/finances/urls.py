f3132a2e9f6f92eca034ee15060ea14b1326e8e3
skybldev <skybluehexapon@outlook.com>
2025-10-26 01:45:05 -0400

    fix delete behavior

--- apps/finances/urls.py
+++ apps/finances/urls.py
@@ -9,14 +9,14 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("transactions/", views.transactions, name="transactions"),
-    path("reports/", views.reports, name="reports"),
-    # create a url path to edit_transactions and redirect back to transactions
-
     path(
         "transactions/delete/<int:entry_id>/",
         views.delete_transactions,
         name="delete_transactions",
     ),
+    path("reports/", views.reports, name="reports"),
+    # create a url path to edit_transactions and redirect back to transactions
+
     path("categories/", views.categories, name="categories"),
     path(
         "categories/delete/<int:category_id>/",
