4cc80e441b5e8811a3bc76038ccc4c5282b54e3d
Layla Shuemate <92069218+LaylaScout@users.noreply.github.com>
2025-10-29 15:12:54 -0400

    Edit Category, Questions about the goal object in category & database (#28)
    * Edit Category
    
    I commented out the goal object in the class Category in the models.py for this to work. I'm going to have to figure out why the goals is messing up the category class. While I was working I kept running into issues saying the database doesn't have a goal section as an object. Is there a way to add "goal" to the database?
    
    * Edits after review
    
    I created another page for edit_transacations because I couldn't figure out how to do it on the same page. The edit_transactions for redirects back to transactions. I also added an edit_transactions view in the views.py file.
    
    * use native django form for adding transactions
    
    - Entry.entry_type defaults to expense
    - new EntryForm replaces hard-coded form in transactions.html
    - enforce side-by-side layout for transactions page
    
    * use native django form for filtering entries
    
    - entry filter uses a new EntryFilterForm
    - autofill today's date and expense type for EntryForm
    - add minimum amount 0 to EntryForm and EntryFilterForm
    - fix labels for entry_type
    - remove imports and comments I accidentally left in
    - clear button resets filters now
    
    * make edit autofill form instead of redir to edit page
    
    - rename and color the "reset" and "add" buttons
    - cancel button when making an edit
    - editing a transaction scrolls to and highlights it in the table
    - show transaction ID on table and when editing
    - add layout and button styling in base.css
    - remove old transactions/id path
    
    * fix delete behavior
    
    * css and template fixes (transactions, dashboard, base)
    
    - fixed typo shrinking table layout when editing
    - formatted transactions template
    - straighten up navbar
    - show amount in monospace font to keep it consistent
    - make rows slightly thinner and more consisent across dashboard and transactions edit page
    - make sidebar size more predictable
    - polish out css hacks
    - make forms more consistent looking (not touching account forms yet)
    
    * regen migrations after merge
    
    * more layout+css fixes (base, login, signup, dashboard)
    
    - made centered content (like on dashboard and home) share a common style
    - fix up login, signup, and auth to align with the rest of the site
    
    * removed the ID number from the transactions form
    
    I removed the ID# from the transactions table and editing format. The site will no longer display the transaction ID# on the table.
    
    * show appropriate messages when no transactions found
    
    * implement filtering by date range
    
    * Remove .value from entry_type.
    
    * Remove redundant link
    
    * Remove/leave comments and change & to and.
    
    ---------
    
    Co-authored-by: skybldev <skybluehexapon@outlook.com>
    Co-authored-by: Skybbles <jake@skybldev.eu.org>
    Co-authored-by: Kenneth Stancil <githubbeded@proton.me>

--- apps/finances/forms.py
+++ apps/finances/forms.py
@@ -1,12 +1,13 @@
+from datetime import datetime, timezone, date
+
 from django import forms
 
-from .models import Category
-
+from .models import Category, Entry, EntryType
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ["name", "entry_type", "goal"]
+        fields = ["name", "entry_type"] #, "goal"] # removed goal. will need to rework it after category edits are merged
         widgets = {
             "name": forms.TextInput(
                 attrs={
@@ -42,3 +43,106 @@ class CategoryForm(forms.ModelForm):
 #             "entry_type": forms.Select(attrs={"class": "form-select"}),
 #             "goal": forms.NumberInput(attrs={"class": "form-control"}),
 #         }
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ["date", "name", "amount", "entry_type", "category", "description"]
+        
+        widgets = {
+            "date": forms.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "form-control",
+                    "id": "transaction-date",
+                }
+            ),
+            "name": forms.TextInput(
+                attrs={
+                    "placeholder": "Transaction Name",
+                    "class": "form-control",
+                    "id": "transaction-name",
+                }
+            ),
+            "amount": forms.NumberInput(
+                attrs={
+                    "placeholder": "$---.--",
+                    "class": "form-control",
+                    "id": "transaction-amount",
+                    "min": 0
+                }
+            ),
+            "entry_type": forms.RadioSelect(
+                attrs={
+                    "label": "Transaction Type",
+                    "class": "form-select",
+                    "id": "transaction-type",
+                }
+            ),
+            "category": forms.Select(
+                attrs={
+                    "class": "form-select",
+                    "id": "transaction-category",
+                }
+            ),
+            "description": forms.TextInput(
+                attrs={
+                    "placeholder": "Description",
+                    "class": "form-control",
+                    "id": "transaction-description",
+                    "rows": 3,
+                }
+            ),
+        }
+
+class EntryFilterForm(forms.Form):
+    date_start = forms.DateField(
+        label="From",
+        required=False,
+        initial=None,
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+    date_end = forms.DateField(
+        label="To",
+        required=False,
+        initial=None,
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+    name = forms.CharField(label="Transaction Name", required=False)
+    amount = forms.IntegerField(
+        label="Amount",
+        required=False,
+        initial=None,
+        widget=forms.NumberInput(attrs={"placeholder": "$---.--"}),
+        min_value=0
+    )
+    entry_type_income = forms.BooleanField(
+        label="Income Transactions",
+        required=False,
+        initial=True,
+    )
+    entry_type_expense = forms.BooleanField(
+        label="Expense Transactions",
+        required=False,
+        initial=True,
+    )
+    category = forms.ChoiceField(
+        required=False,
+        initial=None
+    )
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        date_start = cleaned_data.get("date_start")
+        date_end = cleaned_data.get("date_end")
+
+        if date_start and date_end:
+            if date_start > date_end:
+                self.add_error("date_start", "The start date must be earlier than the end date.")
+            
+            if date_start > date.today():
+                self.add_error("date_start", "The start date cannot be in the future.")
+
+            if date_end > date.today():
+                self.add_error("date_end", "The end date cannot be in the future.")
+                
