e29cddfd3b0b66384a3c278128481f6dafd91042
Kenneth Stancil <githubbeded@proton.me>
2025-10-31 20:51:05 -0400

    Refactor code

--- apps/finances/static/finances/js/goals.js
+++ apps/finances/static/finances/js/goals.js
@@ -1,124 +1,6 @@
-// Main screen
-const add_btn = document.querySelector("#account_goals_add");
-const edit_btn = document.querySelector("#account_goals_edit");
-const open_delete_btn = document.querySelector("#account_goals_delete"); // Opens delete dialog
-
-// Message Dialog
-let msg_dialog = document.querySelector("#msg_dialog")
-let msg = document.querySelector("#msg")
-let msg_close_btn = document.querySelector("#msg_close")
-msg_close_btn.addEventListener('click', function() {
-    msg_dialog.close();
-});
-
-// AccountGoal add dialog
-const add_dialog = document.querySelector("#add_dialog");
-const add_goal_cancel_btn = document.querySelector("#add_goal_cancel");
-add_btn.addEventListener('click', function() {
-    add_dialog.showModal();
-});
-add_goal_cancel_btn.addEventListener('click', function() {
-    add_dialog.close();
-});
-// Show add dialog if an error is present in the form, set by view
-if (add_dialog.dataset.showOnLoad === "true") {
-    add_dialog.showModal();
-}
-
-// AccountGoal edit dialog
-const edit_dialog = document.querySelector("#edit_dialog");
-const edit_goal_cancel_btn = document.querySelector("#edit_goal_cancel");
-
-edit_btn.addEventListener('click', function() {
-    let selected_acct_goals = document.querySelectorAll(".acct_goal.selected");
-    
-    if(selected_acct_goals.length == 0) {
-        msg.textContent = "Click on an account goal to select it for editing.";
-        msg_dialog.showModal();
-    }
-    else if(selected_acct_goals.length > 1) {
-        msg.textContent = "Select only one account goal to edit.";
-        msg_dialog.showModal();
-    }
-    else {
-        // Get goal id and use it as a url parameter to send to the view for editing
-        let goal_id = selected_acct_goals[0].getAttribute("goal_id");
-        window.location.href = `/finances/goals?edit=${goal_id}`;
-    }
-});
-
-edit_goal_cancel_btn.addEventListener('click', function() {
-    edit_dialog.close();
-  });
-
-if(edit_dialog.dataset.showOnLoad === "true") {
-    edit_dialog.showModal();
-}
-
-// AccountGoal delete dialog
-const delete_dialog = document.querySelector("#deletion_dialog");
-const delete_cancel_btn = document.querySelector("#delete_cancel");
-const delete_confirm_btn = document.querySelector("#delete_confirm");
-open_delete_btn.addEventListener('click', function() {
-    let selectedAcctGoals = document.querySelectorAll(".acct_goal.selected");
-    if(selectedAcctGoals.length == 0) {
-        msg.textContent = "Select at least one account goal to delete."
-        msg_dialog.showModal()
-    }
-    else {
-        delete_dialog.showModal();
-    }
-    
-});
-delete_cancel_btn.addEventListener('click', function() {
-    delete_dialog.close();
-});
-
-
-// Category goals filter
-const category_filter = document.querySelector("#category_filter");
-
-// Function to show only the selected category's goals (or all category goals)
-function filterCatGoals() {
-    const selected_category = category_filter.value;
-    const rows = document.querySelectorAll(".cat_goal");
-    // Row that shows when there are no categroy goals to show
-    const no_cat_row = document.querySelector("#no_cat_goals")
-    // Hide row again if it was shown by the previous selection
-    no_cat_row.style.display = "none";
-
-    let rows_shown = 0;
-    rows.forEach(row => {
-        // If selected category is empty ("All Categories" value is "") or the category is correct show the goal's row
-        if (!selected_category || row.dataset.categoryId === selected_category) {
-            row.style.display = "";  // Show
-            ++rows_shown;
-        } else {
-            row.style.display = "none";  // Hide
-        }
-    });
-    if(rows_shown === 0) {
-        no_cat_row.style.display = "";
-    }
-};
-// Run the filter to make sure all the initially shown rows are correct then add an event listener to the select element
-filterCatGoals();
-category_filter.addEventListener('change', filterCatGoals);
-
-
-
-// Add an event listener to each row that represents a goal to select it when clicked
-const rows = document.querySelectorAll("tr[goal_id]");
-
-rows.forEach(row => {
-    row.addEventListener('click', function() {
-        this.classList.toggle('selected');
-    });
-});
-
-
-
-
+// ============================================
+// SHARED UTILITIES
+// ============================================
 // Define a function to get a browser cookie of name name
 function getCookie(name) {
     let cookieArr = document.cookie.split(";");
@@ -133,36 +15,37 @@ function getCookie(name) {
         }
     }
 
-    return null
+    return null;
+}
+
+function getSelectedGoals(className) {
+    return document.querySelectorAll(`.${className}.selected`);
 }
 
 // Send account goal ids and category goal ids to the goal deletion view.
-function sendGoalIds() {
-    let selectedAcctGoals = document.querySelectorAll(".acct_goal.selected");
-    let selectedCatGoals = document.querySelectorAll(".cat_goal.selected");
-    let aGoalIdsToRm = [];
-    let cGoalIdsToRm = [];
+function sendGoalIds(goals, goalType) {
+    let goalIdsToRm = [];
 
-    // Add account goals to list of account goals to remove
-    selectedAcctGoals.forEach(element => {
-        let id = element.getAttribute("goal_id");
-        aGoalIdsToRm.push(id);
+    // Get ids from the goals to send to the django view
+    goals.forEach(element => {
+        let id = element.getAttribute("goal-id");
+        goalIdsToRm.push(id);
     });
-
-    // Add category goals to list of category goals to remove
-    selectedCatGoals.forEach(element => {
-        let id = element.getAttribute("goal_id");
-        cGoalIdsToRm.push(id);
-    });
-
-    // Send both lists to the goal deletion view then deal with the response
+    let body = "";
+    if(goalType === "account") {
+        body = JSON.stringify({acctGoals: goalIdsToRm});
+    }
+    else if(goalType === "category") {
+        body = JSON.stringify({catGoals: goalIdsToRm});
+    }
+    // Send the id list to the goal deletion view then deal with the response
     fetch('/finances/goals/delete/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'X-CSRFToken': getCookie('csrftoken'),
         },
-        body: JSON.stringify({acctGoals: aGoalIdsToRm, catGoals: cGoalIdsToRm}),
+        body: body,
     }).then(response => {
         if(!response.ok) {
             throw new Error("Server response was not ok");
@@ -177,5 +60,161 @@ function sendGoalIds() {
     });
 }
 
-// Delete selected goals
-delete_confirm_btn.addEventListener('click', sendGoalIds);
+
+// ============================================
+// SHARED DIALOGS
+// ============================================
+// Message Dialog
+let msg_dialog = document.querySelector("#msg-dialog");
+let msg = document.querySelector("#msg");
+let msg_close_btn = document.querySelector("#msg-close");
+msg_close_btn.addEventListener('click', function() {
+    msg_dialog.close();
+});
+
+// Goal delete dialog
+const goal_delete_dialog = document.querySelector("#goal-delete-dialog");
+const delete_cancel_btn = document.querySelector("#goal-delete-cancel");
+const delete_confirm_btn = document.querySelector("#goal-delete-confirm");
+
+// Store what to delete when delete confirm is clicked
+let pendingDeletion = null;
+
+// Button for closing the deletion dialog, also clears the list of elements to delete
+delete_cancel_btn.addEventListener('click', function() {
+    goal_delete_dialog.close();
+    pendingDeletion = null;
+});
+
+// Button for confirming deletion, only does anything if there are actually things to delete
+delete_confirm_btn.addEventListener('click', function() {
+    if(pendingDeletion) {
+        sendGoalIds(pendingDeletion.goals, pendingDeletion.type);
+        pendingDeletion = null;
+    }
+});
+
+
+// ============================================
+// ACCOUNT GOALS
+// ============================================
+// Account goals table buttons
+const acct_goal_add_btn = document.querySelector("#acct-goal-add-btn");
+const acct_goal_edit_btn = document.querySelector("#acct-goal-edit-btn");
+const acct_open_delete_btn = document.querySelector("#acct-goal-delete-btn"); // Opens delete dialog
+
+// AccountGoal add dialog
+const acct_goal_add_dialog = document.querySelector("#acct-goal-add-dialog");
+const acct_goal_add_cancel_btn = document.querySelector("#acct-goal-add-cancel");
+acct_goal_add_btn.addEventListener('click', function() {
+    acct_goal_add_dialog.showModal();
+});
+acct_goal_add_cancel_btn.addEventListener('click', function() {
+    acct_goal_add_dialog.close();
+});
+// Show account goal add dialog if an error is present in the form, set by view
+if (acct_goal_add_dialog.dataset.showOnLoad === "true") {
+    acct_goal_add_dialog.showModal();
+}
+
+// AccountGoal edit dialog
+const acct_goal_edit_dialog = document.querySelector("#acct-goal-edit-dialog");
+const acct_goal_edit_cancel_btn = document.querySelector("#acct-goal-edit-cancel");
+
+acct_goal_edit_btn.addEventListener('click', function() {
+    let selected_acct_goals = getSelectedGoals("acct-goal-row");
+    
+    if(selected_acct_goals.length == 0) {
+        msg.textContent = "Click on an account goal to select it for editing.";
+        msg_dialog.showModal();
+    }
+    else if(selected_acct_goals.length > 1) {
+        msg.textContent = "Select only one account goal to edit.";
+        msg_dialog.showModal();
+    }
+    else {
+        // Get goal id and use it as a url parameter to send to the view for editing
+        let goal_id = selected_acct_goals[0].getAttribute("goal-id");
+        window.location.href = `/finances/goals?edit=${goal_id}`;
+    }
+});
+
+acct_goal_edit_cancel_btn.addEventListener('click', function() {
+    acct_goal_edit_dialog.close();
+});
+
+// Show account goal edit dialog if an error is present in the form, set by view
+if(acct_goal_edit_dialog.dataset.showOnLoad === "true") {
+    acct_goal_edit_dialog.showModal();
+}
+
+// AccountGoal delete dialog setup
+acct_open_delete_btn.addEventListener('click', function() {
+    let selectedAcctGoals = getSelectedGoals("acct-goal-row");
+    if(selectedAcctGoals.length == 0) {
+        msg.textContent = "Select at least one account goal to delete.";
+        msg_dialog.showModal();
+    }
+    else {
+        // Store selected account goals to delete
+        pendingDeletion = {
+            goals: selectedAcctGoals,
+            type: "account"
+        };
+        goal_delete_dialog.showModal();
+    }
+    
+});
+
+
+// ============================================
+// CATEGORY GOALS  
+// ============================================
+// Category goals table buttons
+const cat_goal_add_btn = document.querySelector("#category-goals-add");
+const cat_goal_edit_btn = document.querySelector("#category-goals-edit");
+const cat_open_delete_btn = document.querySelector("#category-goals-delete");
+
+// Category goals filter
+const category_filter = document.querySelector("#category-filter");
+
+// Function to show only the selected category's goals (or all category goals)
+function filterCatGoals() {
+    const selected_category = category_filter.value;
+    const rows = document.querySelectorAll(".cat-goal-row");
+    // Row that shows when there are no category goals to show
+    const no_cat_goals_row = document.querySelector("#no-cat-goals-row");
+    // Hide row again if it was shown by the previous selection
+    no_cat_goals_row.style.display = "none";
+
+    let rows_shown = 0;
+    rows.forEach(row => {
+        // If selected category is empty ("All Categories" value is "") or the category is correct show the goal's row
+        if (!selected_category || row.dataset.categoryId === selected_category) {
+            row.style.display = "";  // Show
+            ++rows_shown;
+        } else {
+            row.style.display = "none";  // Hide
+        }
+    });
+    if(rows_shown === 0) {
+        no_cat_goals_row.style.display = "";
+    }
+}
+// Run the filter to make sure all the initially shown rows are correct then add an event listener to the select element
+filterCatGoals();
+category_filter.addEventListener('change', filterCatGoals);
+
+
+
+// ============================================
+// OTHER
+// ============================================
+// Add an event listener to each row that represents a goal to select it when clicked
+const rows = document.querySelectorAll("tr[goal-id]");
+
+rows.forEach(row => {
+    row.addEventListener('click', function() {
+        this.classList.toggle('selected');
+    });
+});
