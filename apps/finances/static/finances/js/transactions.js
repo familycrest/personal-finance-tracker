76f57b515f4c48294e9d242559f23b171304ded1
Kenneth Stancil <githubbeded@proton.me>
2025-11-04 19:12:19 -0500

    Fix reset filters button in filter transactions form.

---
+++ apps/finances/static/finances/js/transactions.js
@@ -0,0 +1,5 @@
+const filterTransRstBtn = document.querySelector("#filter-trans-rst-btn");
+
+filterTransRstBtn.addEventListener('click', function() {
+    window.location.href="/finances/transactions/";
+});
