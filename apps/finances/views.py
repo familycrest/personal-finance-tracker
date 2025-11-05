3cca4b9b54c213d7c0d7f0069fcd45afdc5c785f
Kenneth Stancil <githubbeded@proton.me>
2025-11-04 19:15:03 -0500

    Remove imports I forgot to remove earlier when I moved some time calculation logic to forms.py.

--- apps/finances/views.py
+++ apps/finances/views.py
@@ -1,5 +1,4 @@
-from datetime import datetime, date, timedelta
-from calendar import monthrange
+from datetime import datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -16,7 +15,6 @@ from .forms import (  # import forms
     EditCategoryGoalForm,
     )
 from django.db.models import Sum  # import the sum module so the date range can be calculated
-from django.utils import timezone  # import timezone for date
 from django.http import JsonResponse
 import json
 
