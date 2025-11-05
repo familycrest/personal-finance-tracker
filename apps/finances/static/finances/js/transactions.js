a43d36786138dc71eab5070f7595b589b741709d
Kenneth Stancil <githubbeded@proton.me>
2025-11-04 22:46:56 -0500

    Fix hard coded urls for a couple tempates / JS files.

--- apps/finances/static/finances/js/transactions.js
+++
@@ -1,5 +0,0 @@
-const filterTransRstBtn = document.querySelector("#filter-trans-rst-btn");
-
-filterTransRstBtn.addEventListener('click', function() {
-    window.location.href="/finances/transactions/";
-});
