f27d672eaba924dcdda4b1106895a4ba5cc36b5a
skybldev <skybluehexapon@outlook.com>
2025-11-09 09:31:55 -0500

    delete old dashboard page

--- base/views.py
+++ base/views.py
@@ -9,29 +9,6 @@ from django.http import HttpRequest
 def home(request):
     return render(request, "home.html")
 
-
-@login_required
-def dashboard(request: HttpRequest):
-    banner_message = None
-
-    """
-    # gonna finish this later; it should be using url params instead of header. it's an optional, low prio feature
-    if :
-        parsed_referer = urlparse(referer)
-        referer_path = parsed_referer.path
-        
-        print(referer_path)
-        
-        match referer_path:
-            case "/accounts/auth":
-                banner_message = "You have successfully been authenticated and signed in!"
-            case "/accounts/signup" | "/accounts/login":
-                banner_message = "You are already logged into an account."
-    """
-
-    return render(request, "dashboard.html", {"login_message": banner_message})
-
-
 # View for invalid requests
 def custom_404(request, exception):
     return render(request, "404.html", status=404)
