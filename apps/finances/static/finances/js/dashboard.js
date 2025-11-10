52f3a323228ef81ef9f19da3596ec145c5ef6538
Layla Shuemate <sjshuemate@gmail.com>
2025-11-10 17:45:48 -0500

    created a financial quotes box for fun! I think it'll give the website personality.

---
+++ apps/finances/static/finances/js/dashboard.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+//const periodFilterRstBtn = document.querySelector("#period-filter-rst-btn");
+//periodFilterRstBtn.addEventListener('click', function() {
+//    window.location.href="{% url 'dashboard' %}";
+//});
+//
+//
+//document.addEventListener("DOMContentLoaded", function() {
+//    const quotes = [
+//        "When does it rain money? When there's change in the air",
+//        "My wallet is like an onion, opening it makes me cry.",
+//        "Girls just wanna have funds.",
+//        "My friends say I'm cheap, but I'm not buying it.",
+//        "What did the duck say after going shopping? Put it on my bill.",
+//        "If money doesn't grow on trees, then why do banks have branches?",
+//        "What do you call a belt made of $100? A waist of money.",
+//        "Why is money called dough? Because we need it.",
+//        "Knock, knock. Who's there? I.O... I.O who? Me, when can pay me back?",
+//        "Where do polar bears keep their money? At a snow bank.",
+//        "What do you call pasts with no money? Penne-less.",
+//        "Knock, knock. Who's there? Cash... Cash who? No, thanks, I prefer peanuts",
+//        "Why are money jokes funny? Because they make cents!",
+//        "Pigeons are the richest birds. They don't mind making deposits on expensive cars.",
+//        "I wish my wallet came with free refills!",
+//        "Why did the robber take a bath before he robbed the bank? To make a clean getaway.",
+//    ];
+//
+//    // reference the box by its ID
+//    const box = document.getElementById("quoteBox");
+//
+//    // a random quote will be chosen each time the page loads
+//    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+//    box.textContent = randomQuote;
+//});
