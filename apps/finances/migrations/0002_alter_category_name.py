894951252b99062f5aa53f91f36a3597899ab4fb
Kenneth Stancil <githubbeded@proton.me>
2025-12-01 21:56:32 -0500

    Lint file and get rid of unused migrations

--- apps/finances/migrations/0002_alter_category_name.py
+++ apps/finances/migrations/0002_alter_category_name.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('finances', '0001_initial'),
+        ("finances", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='category',
-            name='name',
+            model_name="category",
+            name="name",
             field=models.CharField(max_length=20),
         ),
     ]
