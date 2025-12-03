952be7d97c8f31981c47aa00fa0347afa3c04090
Kenneth Stancil <githubbeded@proton.me>
2025-12-02 22:21:41 -0500

    Remove commented out code and fix other code to work with changes in test_goal_history.

--- apps/finances/tests/test_goal_history.py
+++ apps/finances/tests/test_goal_history.py
@@ -10,12 +10,6 @@ User = get_user_model()
 
 class GoalHistoryModelTest(TestCase):
     def setUp(self):
-        # clean up all tables before each test
-        User.objects.all().delete()
-        Category.objects.all().delete()
-        AccountGoal.objects.all().delete()
-        CategoryGoal.objects.all().delete()
-
         self.user = User.objects.create_user(
             username="testuser", email="testexample@gmail.com", password="testpassword"
         )
@@ -56,63 +50,6 @@ class GoalHistoryModelTest(TestCase):
 
     def test_category_goal_entry_type_auto_set(self):
         self.assertEqual(self.category_goal.entry_type, self.category.entry_type)
-
-    # def test_account_goal_progress(self):
-    #     # Create entries that contribute to the account goal
-    #     Entry.objects.create(
-    #         name="Test Entry 1",
-    #         user=self.user,
-    #         entry_type=EntryType.INCOME,
-    #         amount=200.00,
-    #         date=date(2023, 3, 15),
-    #     )
-    #     Entry.objects.create(
-    #         name="Test Entry 2",
-    #         user=self.user,
-    #         entry_type=EntryType.INCOME,
-    #         amount=300.00,
-    #         date=date(2023, 5, 20),
-    #     )
-
-    #     progress = self.account_goal.progress
-    #     self.assertEqual(progress, 50.0)  # (200 + 300) / 1000 * 100 = 50%
-
-    # def test_category_goal_progress(self):
-    #     # create new category for 2023
-    #     self.category_2023 = Category.objects.create(
-    #         user=self.user,
-    #         name="Test Category 2023",
-    #         entry_type=EntryType.INCOME,
-    #     )
-    #     self.category_goal_2023 = CategoryGoal.objects.create(
-    #         category=self.category_2023,
-    #         name="Test Category Goal 2023",
-    #         amount=500.00,
-    #         start_date=date(2023, 1, 1),
-    #         end_date=date(2023, 6, 30),
-    #     )
-
-    #     # Create entries that contribute to the category goal
-    #     Entry.objects.create(
-    #         name="Test Entry 3",
-    #         user=self.user,
-    #         category=self.category_2023,
-    #         entry_type=self.category_2023.entry_type,
-    #         amount=150.00,
-    #         date=date(2023, 2, 10),
-    #     )
-    #     Entry.objects.create(
-    #         name="Test Entry 4",
-    #         user=self.user,
-    #         category=self.category_2023,
-    #         entry_type=self.category_2023.entry_type,
-    #         amount=100.00,
-    #         date=date(2023, 4, 5),
-    #     )
-
-    #     progress = self.category_goal.progress()
-    #     self.assertEqual(self.category_goal.amount, 500.00)
-    #     self.assertEqual(progress, 50.0)  # (150 + 100) / 500 * 100 = 50%
 
     def test_goal_progress_with_no_entries(self):
         progress = self.account_goal.progress
@@ -201,13 +138,14 @@ class GoalHistoryViewTests(TestCase):
 
         # Now the list should contain 2 goals
         self.assertEqual(len(response.context["goals_output"]), 2)
-        goal_names = [goal["name"] for goal in response.context["goals_output"]]
+        goal_names = [goal.name for goal in response.context["goals_output"]]
         self.assertIn("Test Account Goal 1", goal_names)
         self.assertIn("Test Category Goal 1", goal_names)
 
         # Check that category name is included for CategoryGoal
         for goal in response.context["goals_output"]:
-            if goal["name"] == "Test Category Goal 1":
-                self.assertEqual(goal["category_name"], "Test Category")
+            if goal.name == "Test Category Goal 1":
+                self.assertEqual(goal.category.name, "Test Category")
             else:
-                self.assertIsNone(goal.get("category_name"))
+                with self.assertRaises(AttributeError):
+                    goal.category.name
