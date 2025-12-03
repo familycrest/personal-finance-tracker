152524834db2c423b6aa758210ae8c1be46120db
Kenneth Stancil <githubbeded@proton.me>
2025-12-02 22:18:01 -0500

    Remove code  that relies on a condition that can never happen because self.category is required in CategoryGoal.

--- apps/finances/models.py
+++ apps/finances/models.py
@@ -183,6 +183,4 @@ class CategoryGoal(Goal):
 
     # return the category goal by name
     def __str__(self):
-        if not self.category:
-            return self.name
         return f"{self.name} ({self.category.name})"
